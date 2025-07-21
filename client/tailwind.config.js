d4622096ba62d6e0190cfed4407ec2b659919b53
robinahmed12 <shawonahmed0294@gmail.com>
2025-07-21 23:59:22 +0600

    add tailwind.config.js for controlling os color

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: false, // disables dark mode completely
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {}, // extend your custom styles here
+  },
+  plugins: [],
+};
